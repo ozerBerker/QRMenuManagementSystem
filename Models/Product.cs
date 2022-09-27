0c00230e363e7ec34660e29c32309b5f3acb0b90
ozerberker19@gmail.com <ozerberker19@gmail.com>
2022-09-28 02:57:26 +0300

    Complete Category and Product controllers and connect them together

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace QRMenuManagementSystem.Models
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public double ProductPrice { get; set; }
+        public Category category { get; set; }
+        public string CategoryId { get; set; }
+
+    }
+}
